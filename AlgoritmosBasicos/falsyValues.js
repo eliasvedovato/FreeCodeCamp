10f0515eb77b613e108304c0c9567b4c93318657
Elias Vedovato <eliasvedovato@gmail.com>
2022-09-01 23:10:34 -0300

    casi terminando algoritmos

---
+++ AlgoritmosBasicos/falsyValues.js
@@ -0,0 +1,21 @@
+/* Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN. */
+
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) newArray.push(arr[i]);
+        /* We use the if statement to check if the current element is truthy or falsy
+        If the element is truthy, we push it to the new array (newArray). This result in the new 
+        array containing only truthy elements. */
+    }
+    return newArray;
+}
+
+function bouncer(arr) {
+	return arr.filter(Boolean)
+}
+
+/* he Array.prototype.filter method expects a function that returns a Boolean value which 
+takes a single argument and returns true for truthy 2.7k value or false for falsy 4.3k value */
